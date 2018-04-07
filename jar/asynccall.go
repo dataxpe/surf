fece9caa99583448ff280673b0241fc035a189b2
dolphi iapon <evgeniysolomanidin@gmail.com>
2018-04-07 21:33:07 +0300

    add time

--- jar/asynccall.go
+++ jar/asynccall.go
@@ -2,28 +2,33 @@ package jar
 
 import (
 	"sync"
+	"time"
 
 	"github.com/Diggernaut/goquery"
 )
 
+type AsyncDom struct {
+	T time.Time
+	D *goquery.Document
+}
 type AsyncStore struct {
 	sync.Mutex
-	doms map[string]*goquery.Document
+	doms map[string]*AsyncDom
 }
 
 func NewAsyncStore() *AsyncStore {
-	return &AsyncStore{doms: make(map[string]*goquery.Document)}
+	return &AsyncStore{doms: make(map[string]*AsyncDom)}
 }
 func (a *AsyncStore) Set(name string, val *goquery.Document) {
 	a.Lock()
 	defer a.Unlock()
-	a.doms[name] = val
+	a.doms[name] = &AsyncDom{T: time.Now(), D: val}
 }
-func (a *AsyncStore) Get(name string) *goquery.Document {
+func (a *AsyncStore) Get(name string) *AsyncDom {
 	a.Lock()
 	defer a.Unlock()
 	if v, ok := a.doms[name]; ok {
 		return v
 	}
-	return &goquery.Document{}
+	return &AsyncDom{}
 }
